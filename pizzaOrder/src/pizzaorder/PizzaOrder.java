9edf97ce10965aa66724d44a961157d39557d60c
avarela67299 <alejovarela98@hotmail.com>
2023-03-09 14:43:56 -0800

    finished interface

--- pizzaOrder/src/pizzaorder/PizzaOrder.java
+++ pizzaOrder/src/pizzaorder/PizzaOrder.java
@@ -18,6 +18,27 @@ public class PizzaOrder extends javax.swing.JFrame {
     JRadioButton thinRadioButton = new JRadioButton();
     JRadioButton thickRadioButton = new JRadioButton();
     
+    //toppings panel 
+    JPanel toppingsPanel = new JPanel();
+    JCheckBox cheeseCheckBox = new JCheckBox();
+    JCheckBox mushroomsCheckBox = new JCheckBox();
+    JCheckBox olivesCheckBox = new JCheckBox();
+    JCheckBox onionsCheckBox = new JCheckBox();
+    JCheckBox peppersCheckBox = new JCheckBox();
+    JCheckBox tomatoesCheckBox = new JCheckBox();
+    
+    //dining panel
+    JPanel diningPanel = new JPanel();
+    ButtonGroup diningButtonGroup = new ButtonGroup();
+    JRadioButton eatInButton = new JRadioButton();
+    JRadioButton eatOutButton = new JRadioButton();
+    
+    // butons
+    JButton orderButton = new JButton();
+    JButton historyButton = new JButton();
+    
+    
+    
 
     public static void main(String[] args) {
         new PizzaOrder().show();
@@ -118,7 +139,119 @@ public class PizzaOrder extends javax.swing.JFrame {
         // add crust panel to the frame
         gridConstraints.gridx = 0;
         gridConstraints.gridy = 1;
+        gridConstraints.gridheight = 2;
         getContentPane().add(crustPanel,gridConstraints);
+        
+        // toppings panel layout 
+        toppingsPanel.setLayout(new GridBagLayout());
+        toppingsPanel.setBorder(BorderFactory.createTitledBorder("Toppings")); 
+        
+        // add contros to toppings panel
+        cheeseCheckBox.setText("Extra Cheese");
+        gridConstraints.gridx = 0;
+        gridConstraints.gridy = 0;
+        gridConstraints.gridheight = 1;
+        gridConstraints.anchor = GridBagConstraints.WEST;
+        toppingsPanel.add(cheeseCheckBox, gridConstraints);
+        
+        mushroomsCheckBox.setText("Mushrooms");
+        gridConstraints.gridx = 0;
+        gridConstraints.gridy = 1;
+        gridConstraints.anchor = GridBagConstraints.WEST;
+        toppingsPanel.add(mushroomsCheckBox, gridConstraints);
+        
+        olivesCheckBox.setText("Olives");
+        gridConstraints.gridx = 0;
+        gridConstraints.gridy = 2;
+        gridConstraints.anchor = GridBagConstraints.WEST;
+        toppingsPanel.add(olivesCheckBox, gridConstraints);
+        
+        onionsCheckBox.setText("Onions");
+        gridConstraints.gridx = 1;
+        gridConstraints.gridy = 0;
+        gridConstraints.anchor = GridBagConstraints.WEST;
+        toppingsPanel.add(onionsCheckBox, gridConstraints);
+        
+        peppersCheckBox.setText("Green Peppers");
+        gridConstraints.gridx = 1;
+        gridConstraints.gridy = 1;
+        gridConstraints.anchor = GridBagConstraints.WEST;
+        toppingsPanel.add(peppersCheckBox, gridConstraints);
+        
+        tomatoesCheckBox.setText("Tomatoes");
+        gridConstraints.gridx = 1;
+        gridConstraints.gridy = 2;
+        gridConstraints.anchor = GridBagConstraints.WEST;
+        toppingsPanel.add(tomatoesCheckBox, gridConstraints); 
+        
+        //add toppings panel to frame
+        gridConstraints.gridx = 1;
+        gridConstraints.gridy = 0;
+        gridConstraints.gridwidth = 2;
+        getContentPane().add(toppingsPanel, gridConstraints);
+        
+        //set dining panel layout 
+        diningPanel.setLayout(new GridBagLayout());
+        //sizePanel.setBorder(BorderFactory.createTitledBorder("Size "));
+        
+        //add controls to dining panel
+        eatInButton.setText("Eat In");
+        eatInButton.setSelected(true);
+        gridConstraints.gridx = 0;
+        gridConstraints.gridy = 0;
+        gridConstraints.gridwidth = 1;
+        gridConstraints.anchor = GridBagConstraints.WEST;
+        diningButtonGroup.add(eatInButton);
+        diningPanel.add(eatInButton, gridConstraints);
+        eatInButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+                diningRadioButtonActionPerformed(e);
+            } 
+        });
+        
+        eatOutButton.setText("Eat Out");
+        gridConstraints.gridx = 0;
+        gridConstraints.gridy = 1;
+        gridConstraints.anchor = GridBagConstraints.EAST;
+        diningButtonGroup.add(eatOutButton);
+        diningPanel.add(eatOutButton, gridConstraints);
+        eatOutButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+                diningRadioButtonActionPerformed(e);
+            } 
+        });
+        
+        //add dining panel to frame
+        gridConstraints.gridx = 1;
+        gridConstraints.gridy = 1;
+        gridConstraints.gridheight = 2;
+        gridConstraints.anchor = GridBagConstraints.WEST;
+        getContentPane().add(diningPanel, gridConstraints);
+        
+        //add buttons in frame      
+        orderButton.setText("Order");
+        gridConstraints.gridx = 2;
+        gridConstraints.gridy = 1;
+        gridConstraints.gridheight = 1;
+        gridConstraints.anchor = GridBagConstraints.CENTER;
+        getContentPane().add(orderButton, gridConstraints);
+        orderButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+                orderButtonActionPerformed(e);
+            }       
+        });
+        
+        historyButton.setText("history");
+        gridConstraints.gridx = 2;
+        gridConstraints.gridy = 2;
+        gridConstraints.anchor = GridBagConstraints.CENTER;
+        getContentPane().add(historyButton, gridConstraints);
+        historyButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+                historyButtonActionPerformed(e);
+            }       
+        });
+        
         pack();
         
         //place the program in the middle of the screen
@@ -134,6 +267,15 @@ public class PizzaOrder extends javax.swing.JFrame {
             }
     
     private void crustRadioButtonActionPerformed(ActionEvent e){
+    }
+    
+    private void diningRadioButtonActionPerformed(ActionEvent e) {
+    }
+    
+    private void orderButtonActionPerformed(ActionEvent e) {       
+    }
+    
+    private void historyButtonActionPerformed(ActionEvent e) {      
     }
     
     private void exitForm(WindowEvent e){
